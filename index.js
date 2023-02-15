4393bb93483fb6b92d4998c4e2a8be07ea519e11
Universe <williamsthehack@gmail.com>
2023-02-16 00:44:31 +0100

    up

---
+++ index.js
@@ -0,0 +1,36 @@
+import { ChatGPTAPIBrowser } from "chatgpt"
+const express = require('express');
+const app = express()
+const PORT = 7780;
+
+app.listen(
+    PORT,
+    () => console.log("Its alive on http://localhost:" + PORT + "")
+)
+
+const api = new ChatGPTAPIBrowser({
+    email: "johann.williams@gmx.de",
+    password: "johannw2004"
+})
+await api.initSession()
+
+process.on('uncaughtException', err => {
+    console.error(err && err.stack)
+});
+
+async function ai(q) {
+    const result = await api.sendMessage(q)
+    console.log(result)
+    console.log(result.response)
+    return result.response;
+}
+
+app.get("/gpt/:question", (req, res) => {
+    var { question } = req.params;
+    var result = ai (question)
+  
+    res.status(200).json({
+        message: result
+    })
+})
+
